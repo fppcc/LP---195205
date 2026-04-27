53010133acdb21cb44a61d8eb3293df970519554
Felipe Reis <felipe.reis22@unifesp.br>
2026-04-27 20:36:46 -0300

    2757.C

---
+++ 2757.C
@@ -0,0 +1,20 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Felipe Reis Da Silva
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/2757
+Data        : 27/04/2026
+Objetivo    : criar três variaveis para armazenar número inteiros
+Aprendizado : uso corredo do %d
+-------------------------------------------------------------------------- */
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    printf("A = %d, B = %d, C = %d\n", a, b, c);
+    printf("A = %10d, B = %10d, C = %10d\n", a, b, c);
+    printf("A = %010d, B = %010d, C = %010d\n", a, b, c);
+    printf("A = %-10d, B = %-10d, C = %-10d\n", a, b, c);
+    return 0;
+}
