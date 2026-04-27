16033ce8317310f5e4ce620dc951fa91586ac026
Felipe Reis <felipe.reis22@unifesp.br>
2026-04-27 20:15:26 -0300

    1000.C

---
+++ 1000.C
@@ -0,0 +1,15 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Felipe Reis Da Silva
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1000
+Data        : 27/04/2026
+Objetivo    : imprimir a mensagem "hello world!"" usando linguagem c
+Aprendizado : uso basico do comando printf para imprimir caracteres
+-------------------------------------------------------------------------- */
+#include <stdio.h>
+
+int main() {
+    printf("Hello World!\n");
+    return 0;
+}
