8249615db4c8ac6b925a86984bb2b17e08f66753
Felipe Reis <felipe.reis22@unifesp.br>
2026-04-30 09:38:24 -0300

    1078.C

---
+++ 1078.C
@@ -0,0 +1,24 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Felipe Reis Da Silva
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1078
+Data        : 27/04/2026
+Objetivo    : realizar a tabuada de um número
+Aprendizado : manipulação de string para realizar a tarefa
+-------------------------------------------------------------------------- */
+
+#include <stdio.h>
+int main() {
+    int R [10] , i, n;
+    scanf ("%d", n);
+     if (n>2 && n<1000) {
+        (i=0, i<10, i++);
+        r[i] = (i+1 * n);
+    }
+    else{
+        printf ("numeros incompatives com o programa");
+    }
+        
+ return 0;
+}
